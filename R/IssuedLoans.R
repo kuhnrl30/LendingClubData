eb81861af8ee9b6c2b3868f05cdd47812382e605
kuhnrl30 <kuhnrl30@gmail.com>
2017-08-17 01:35:46 -0400

    Update for 17Q3 data

--- R/IssuedLoans.R
+++ R/IssuedLoans.R
@@ -4,14 +4,20 @@
 #' other variables. Lending Club publishes this data on all loans it has issued
 #' since 2007 and refreshes it quarterly. The data is published approximately
 #' 6 weeks after the quarter.
-#'
+#' @param years numeric vector of years to return 
 #' @return data frame of historical loans
 #'
 #' @export
-IssuedLoans<- function(){
-    x<- get(data(list="LendingClubData_1", envir=environment()))
-    y<- get(data(list="LendingClubData_2", envir=environment()))
+IssuedLoans<- function(years=c(2007:2017)){
+    
+    if( years > 2007 && years > 2017) {
+        stop("Year range is not valid")
+        }
+    
+    
+    year_list= paste0("LendingClubData_",years)
+    x<-plyr::rbind.fill(get(data(list=year_list, package="LendingClubData")))
 
-    as.data.frame(rbind(x,y), stringsAsFactors=F )
-
+    rm(list=year_list, envir=.GlobalEnv)
+    x
 }
